4cbfc14d33c5be3c582f338fd22536ae2f583c7d
Strayneko <bayurendi10@gmail.com>
2023-06-26 15:35:24 +0700

    :zap: add post dashboaard

--- app/Http/Livewire/Dashboard/Index.php
+++ app/Http/Livewire/Dashboard/Index.php
@@ -2,14 +2,38 @@
 
 namespace App\Http\Livewire\Dashboard;
 
+use App\Services\PostService;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
 class Index extends Component
 {
-    use WithFileUploads;
+    public $posts;
+    public $take = self::limit;
 
-    public $file;
+    private PostService $postService;
+    private const limit = 6;
+
+    public function boot(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
+
+    public function mount()
+    {
+        $this->loadPost();
+    }
+
+    public function loadPost(): void
+    {
+        $this->posts = $this->postService->getPosts()?->take($this->take);
+    }
+
+    public function loadMore(): void
+    {
+        $this->take += self::limit;
+        $this->loadPost();
+    }
     public function render()
     {
         return view('livewire.dashboard.index');
